cc2be604c63887321628b74d6dde52c193b6b856
Natalia Barthe <natibarthe29@hotmail.com>
2020-05-14 12:50:27 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { AfterViewInit, Component } from '@angular/core';
+import { defaults as defaultControls } from 'ol/control';
+
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import XYZ from 'ol/source/XYZ';
+import ZoomToExtent from 'ol/control/ZoomToExtent';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+
+export class AppComponent implements AfterViewInit {
+  title: 'OpenLayersDemo';
+  map: Map;
+
+  ngAfterViewInit() {
+    this.map = new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new XYZ({
+            url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+          })
+        })
+      ],
+      view: new View({
+        center: [813079.7791264898, 5929220.284081122],
+        zoom: 7
+      }),
+      controls: defaultControls().extend([
+        new ZoomToExtent({
+          extent: [
+            813079.7791264898, 5929220.284081122,
+            848966.9639063801, 5936863.986909639
+          ]
+        })
+      ])
+    });
+  }
+}
